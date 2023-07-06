10c48f043b34363c10b05b44f2f9a8863c6bfa40
DeboraAve <debora.avelino98@gmail.com>
2023-07-05 22:17:35 -0300

    Incluindo Interações em JS

---
+++ js/scripts.js
@@ -0,0 +1,93 @@
+class Pessoa {
+    nome;
+    idade;
+    linkImagem;
+    bio;
+
+    getNome(){
+        return this.nome;
+    }
+
+    getIdade(){
+        return this.idade;
+    }
+
+    getLinkImagem(){
+        return this.linkImagem;
+    }
+
+    getBio(){
+        return this.bio;
+    }
+
+    constructor(nome, idade, linkImagem, bio){
+        this.nome = nome;
+        this.idade = idade;
+        this.linkImagem = linkImagem;
+        this.bio = bio;
+    }
+}
+
+let atual = 0;
+
+p1 = new Pessoa("Mark", 25, "../img/homem.jpg", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+p2 = new Pessoa("João", 35, "../img/homem2.jpg", "Aenean iaculis velit sit amet nunc convallis semper");
+simulaBanco = [p1,p2];
+
+function preencheDados(){
+    let nome = document.getElementById("nome");
+    let idade = document.getElementById("idade");
+    let bio = document.getElementById("bio");
+    let foto = document.getElementById("foto");
+
+    for(let i = 0; i < simulaBanco.length;i++){
+        if(i != atual){
+            nome.innerHTML = simulaBanco[i].getNome();
+            idade.innerHTML = simulaBanco[i].getIdade();
+            bio.innerHTML = simulaBanco[i].getBio();
+            foto.style.setProperty("background-image", `url("${simulaBanco[i].getLinkImagem()}")`);
+            if(atual == 0){
+                atual = 1;
+                return;
+            }
+            atual = 0;
+            return;
+        }
+    }
+}
+
+function exibirModal(opcao){
+    document.getElementById("modal").style.display = "flex"
+    let mensagem = document.getElementById("mensagem-modal")
+    if (opcao == 1) {
+        mensagem.innerHTML = "PRÓXIMO!!";
+        // document.write("PRÓXIMO!!")
+    }else if (opcao == 2) {
+        mensagem.innerHTML = "SUPER LIKE ENVIADO!!";
+    }else if(opcao == 3) {
+        let sorteio = Math.random();
+        if (sorteio > 0.5)
+            mensagem.innerHTML = "É UM MATCH!!"
+        else
+            mensagem.innerHTML = "LIKE ENVIADO!"
+    }
+}
+
+function naoGostei(){
+    exibirModal(1);
+    preencheDados();
+}
+
+function amar(){
+    exibirModal(2);
+    preencheDados();
+}
+
+function gostar(){
+    exibirModal(3);
+    preencheDados();
+}
+
+function fecharModal(){
+    document.getElementById("modal").style.display = "none";
+}
